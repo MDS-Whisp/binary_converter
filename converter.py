c8a8d94d992277215a66d47df72dfc532d2b8549
Mason Scarborough <masonwhispi@gmail.com>
2024-10-23 17:29:41 +0000

    finished

--- converter.py
+++ converter.py
@@ -1,26 +1,30 @@
 
 
 
-def binary_to_decimal(user_number, binary_number):
+def binary_to_decimal():
     
     binary_number = 15
-    user_number = input(int("input a number 1-15"))
+    user_number = int(input("input a number 1-15: "))
     binary = [0, 0, 0, 0]
 
 
-    if binary_number - 8 <= user_number:
+    if user_number >= 8:
         binary[0] = 1
+        user_number -= 8
 
-        if binary_number - 4 <= user_number:
-            binary[1] = 1
+    if user_number >= 4:
+        binary[1] = 1
+        user_number -= 4
 
-            if binary_number - 2 <= user_number:
-                binary[2] = 1
+    if user_number >= 2:
+        binary[2] = 1
+        user_number -= 2
 
-                if binary_number - 1 <= user_number:
-                    binary[3] = 1
+    if user_number >= 1:
+        binary[3] = 1
+        user_number -= 1
 
-    print(bianry)
+    print(binary)
 
 
-binary_to_decimal(user_number, binary_number)
+binary_to_decimal()
